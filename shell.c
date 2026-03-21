838d5b8cf6a464014f8017165d17dd2823a3c1fc
kf-rust-dev <kf.rust.dev@proton.me>
2026-03-21 23:11:09 +0100

    Add built-in commands: cd, help, exit

--- shell.c
+++ shell.c
@@ -2,11 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 void loop(void);
 char* read_line(void);
 char** parse_line(char* line);
 int execute_command(char** args);
+
+int builtin_cd(char** args);
+int builtin_help(char** args);
+int builtin_exit(char** args);
+
+char* builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_fn[])(char**) = {
+    builtin_cd,
+    builtin_help,
+    builtin_exit
+};
+
+int builtins_num() {
+    return sizeof(builtin_str) / sizeof(char*);
+}
 
 int main(int argc, char** argv) {
     loop();
@@ -95,4 +116,27 @@ char** parse_line(char* line) {
 
     tokens[i] = NULL;
     return tokens;
+}
+
+int builtin_cd(char** args) {
+    if (!args[1]) {
+        fprintf(stderr, "shell: cd: expected argument\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("shell");
+        }
+    }
+    return 1;
+}
+
+int builtin_help(char** args) {
+    printf("shell builtins:\n");
+    for (int i = 0; i < builtins_num(); i++) {
+        printf("  %s\n", builtin_str[i]);
+    }
+    return 1;
+}
+
+int builtin_exit(char** args) {
+    return 0;
 }
